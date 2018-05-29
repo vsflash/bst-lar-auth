fb7ffa239c0f6874b706c15ee6f8e9ca1be6ed55
yura <yura@com.ua>
2018-05-29 18:54:25 +0300

    User and Task relation

--- bst-lar-auth/app/User.php
+++ bst-lar-auth/app/User.php
@@ -4,8 +4,8 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+
     /**
      * The attributes that are mass assignable.
      *
@@ -23,4 +23,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Получить все задачи пользователя.
+     */
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+
 }
